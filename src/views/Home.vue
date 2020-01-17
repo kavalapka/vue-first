351f7257c1640e64c1da45f1b6511b400b447e9b
Katsiaryna Zenchyk <Katsiaryna_Zenchyk@epam.com>
2020-01-17 17:09:38 +0300

    add buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld message="My First Vue.js App" :userMsg="userMsg"
+                :reverseMessage="reverseMessage" :visible="visible" :switchVis="switchVis"
+                :showHide="showHide"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+//
+// const show: 'Show',
+//   hide: 'Hide',
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      visible: true,
+      showHide: 'Hide',
+      userMsg: 'It is user message',
+    };
+  },
+  methods: {
+    reverseMessage() {
+      this.userMsg = this.userMsg.split('').reverse().join('');
+    },
+    switchVis() {
+      this.visible = !this.visible;
+      this.showHide = this.visible ? 'Hide' : 'Show';
+    },
   },
 };
 </script>
