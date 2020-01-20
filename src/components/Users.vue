5a3cafa2d15aa14e7f16ecd398b2445cd656dc80
Katsiaryna Zenchyk <Katsiaryna_Zenchyk@epam.com>
2020-01-20 14:39:32 +0300

    + add Users component

---
+++ src/components/Users.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="users-container">
+    <h3 class="users">Users:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in users" v-bind:key="user.id">
+        <th scope="row">{{user.id}}</th>
+        <td>{{user.name}}</td>
+        <td>{{user.email}}</td>
+        <td>{{user.address.city}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  props: {
+    users: Array,
+  },
+};
+</script>
+
+<style>
+  .users {
+    margin-bottom: 5%;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,15 @@
     <HelloWorld message="My First Vue.js App" :userMsg="userMsg"
                 :reverseMessage="reverseMessage" :visible="visible" :switchVis="switchVis"
                 :showHide="showHide"/>
+    <Users :users="users"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-//
-// const show: 'Show',
-//   hide: 'Hide',
+import Users from '../components/Users.vue';
+
 function reverseMessage() {
   this.userMsg = this.userMsg.split('').reverse().join('');
 }
@@ -25,6 +25,7 @@ function switchVis() {
 export default {
   name: 'home',
   components: {
+    Users,
     HelloWorld,
   },
   data() {
@@ -32,11 +33,17 @@ export default {
       visible: true,
       showHide: 'Hide',
       userMsg: 'It is user message',
+      users: null,
     };
   },
   methods: {
     reverseMessage,
     switchVis,
+  },
+  async created() {
+    const userResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+    this.users = await userResponse.json();
+    console.log('users: ', this.users);
   },
 };
 </script>
